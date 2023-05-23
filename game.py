817f692be1a6e1e129e55a65f03a56a72c4cec07
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-05-23 18:45:21 -0400

    Defined:directory and file structure

---
+++ game.py
@@ -0,0 +1,7 @@
+#main file
+# this function provides  of playing game
+def main():
+    print("Peek_a_Boo")
+
+if __name__ == "__main__":
+    main()
---
+++ grid.py
@@ -0,0 +1,2 @@
+# this file serves as a Grid matrix All the operations will performed here
+# Addtionally this file written in object oriented style with classes
