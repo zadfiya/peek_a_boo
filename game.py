125cda162bf6a6639d9f7489461a4b5fa8347378
Naren Zadafiya <narenzadfiya3579@gmail.com>
2023-05-25 13:21:49 -0400

    menu function defined

--- game.py
+++ game.py
@@ -3,5 +3,14 @@
 def main():
     print("Peek_a_Boo")
 
+def get_menu_choice():
+    print('\nMenu:')
+    print('1. Guess a pair')
+    print('2. Manually uncover a cell')
+    print('3. Reveal the entire grid')
+    print('4. Start a new game')
+    print('5. Quit')
+    return input('Enter your choice: ')
+
 if __name__ == "__main__":
     main()
